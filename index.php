6de17419ebe1aa098baedf88e6543dbf42831ed9
AugustMusande <augustmusande@gmail.com>
2024-02-24 11:34:00 +0100

    formulaire de connexion

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>login_page</title>
+</head>
+<body>
+    <section>
+        <div class="form-box">
+            <div class="form-valuue">
+                <form action="">
+                    <h2>authentification</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="mail" required>
+                        <label for="">user</label>
+                     </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password"  rquired>
+                        <label for="">mot de passe</label>                        
+                    </div>
+                    <div class="forget">
+                        <label for=""><input type="checkbox">me rappeler <a href="">mot de passe oublie?</a></label>
+                    </div> 
+                    <button>soumettre</button>
+                    <div class="register">
+                        <p>j'ai pas un compte je veux  <a href="">m'inscrire</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
